f993aaaba7fd76d66be81847aa594043ce25766e
Dom Rodriguez <shymega@shymega.org.uk>
2024-03-24 14:33:08 +0000

    refactor(devenv): Update & tweak `devenv`

--- devenv.nix
+++ devenv.nix
@@ -5,4 +5,12 @@
   languages.nix.enable = true;
   languages.java.enable = true;
   devcontainer.enable = true;
+  difftastic.enable = true;
+
+  pre-commit.hooks = {
+    actionlint.enable = true;
+    commitizen.enable = true;
+    markdownlint.enable = true;
+    nixpkgs-fmt.enable = true;
+  };
 }
